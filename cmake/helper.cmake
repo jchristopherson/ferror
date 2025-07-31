cdf23cd0bd29853504a2c9c2d39f8681aa4f8295
jchristopherson <jchristopherson@hotmail.com>
2025-07-31 14:29:54 -0500

    Clean up and add install instructions

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -33,30 +33,6 @@ function(add_fortran_library lib_name mod_dir include_install_dir version major)
     )
 endfunction()
 
-# Installs the library
-function(install_library lib_name lib_install_dir bin_install_dir mod_dir install_dir)
-    install(
-        TARGETS ${lib_name}
-        EXPORT ${lib_name}Targets
-        RUNTIME DESTINATION ${bin_install_dir}
-        LIBRARY DESTINATION ${lib_install_dir}
-        ARCHIVE DESTINATION ${lib_install_dir}
-        INCLUDES DESTINATION ${install_dir}/include
-    )
-    install(
-        DIRECTORY ${mod_dir}
-        DESTINATION ${install_dir}
-    )
-endfunction()
-
-# Install the documentation files
-function(install_documentation doc_dir install_dir)
-    install(
-        DIRECTORY ${doc_dir}
-        DESTINATION ${install_dir}
-    )
-endfunction()
-
 # Links the supplied library
 function(link_library targ lib include_dir)
     target_link_libraries(${targ} ${lib})
